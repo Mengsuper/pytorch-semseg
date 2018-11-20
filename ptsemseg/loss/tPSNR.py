b086728bd020bdcb313876ca93f7d6d40e9a05d9
Meng Li <meng@phas.ubc.ca>
2018-11-20 15:53:20 -0800

    add tPSNR loss

---
+++ ptsemseg/loss/tPSNR.py
@@ -0,0 +1,30 @@
+import cv2
+import torch
+import numpy as np
+from torch.autograd import Variable
+from ptsemseg.loss.chromaUpSampling import chromaUpSampling 
+
+
+def tPSNR(input, target):
+
+    input = input.view(input.shape[1], input.shape[2], input.shape[3])
+    input = input.data.numpy()
+    target = target.view(target.shape[1], target.shape[2], target.shape[3])
+    target = target.data.numpy()
+    
+    print (input.shape, target.shape) # (3x360x480, 3x360x480)
+
+    ## need modification 
+    input_rgb = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input]
+    input_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input_rgb]
+
+    target_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB ) for x in target]
+    target_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in target_rgb]
+
+    mse = (np.square(np.array(input_xyz) - np.array(target_xyz))).mean(axis=0)
+    loss = 10*np.log(10*len(lines)/np.sum(mse))
+    ### need modification
+
+    loss = Variable(torch.Tensor(loss), requires_grad=True)
+
+    return loss
