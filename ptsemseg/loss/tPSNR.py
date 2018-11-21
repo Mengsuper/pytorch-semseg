5db9f6e9bc3555a0792f67a6b7e7fbf7b42fcf31
Sugun Machipeddy <sugun.machipeddy@gmail.com>
2018-11-20 17:45:06 -0800

    tpsnr loss

--- ptsemseg/loss/tPSNR.py
+++ ptsemseg/loss/tPSNR.py
@@ -13,16 +13,23 @@ def tPSNR(input, target):
     target = target.data.numpy()
     
     print (input.shape, target.shape) # (3x360x480, 3x360x480)
+    for i in range(len(input[0])):
+        temp1 = []
+        temp2 = []
+        for j in range(1,4):
+            temp1.append(np.unit16(input[j][i]))
+            temp2.append(np.unit16(target[j][i]))
+        input_ycbcr.append(temp1)
+        target_ycbcr.append(temp2)
 
-    ## need modification 
-    input_rgb = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input]
+    input_rgb = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input_ycbcr]
     input_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input_rgb]
 
-    target_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB ) for x in target]
+    target_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB ) for x in target_ycbcr]
     target_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in target_rgb]
-
+    
     mse = (np.square(np.array(input_xyz) - np.array(target_xyz))).mean(axis=0)
-    loss = 10*np.log(10*len(lines)/np.sum(mse))
+    loss = 10*np.log(10*len(input_ycbcr)/np.sum(mse))
     ### need modification
 
     loss = Variable(torch.Tensor(loss), requires_grad=True)
