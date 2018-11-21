edfbb3aad3e916d0ca61eaba3220addf0377c564
Sugun Machipeddy <sugun.machipeddy@gmail.com>
2018-11-20 22:49:50 -0800

    working tpsnr loss

--- ptsemseg/loss/tPSNR.py
+++ ptsemseg/loss/tPSNR.py
@@ -2,7 +2,7 @@ import cv2
 import torch
 import numpy as np
 from torch.autograd import Variable
-from ptsemseg.loss.chromaUpSampling import chromaUpSampling 
+from ptsemseg.loss.chromaUpSampling import chromaUpSampling
 
 
 def tPSNR(input, target):
@@ -11,27 +11,33 @@ def tPSNR(input, target):
     input = input.data.numpy()
     target = target.view(target.shape[1], target.shape[2], target.shape[3])
     target = target.data.numpy()
-    
-    print (input.shape, target.shape) # (3x360x480, 3x360x480)
-    for i in range(len(input[0])):
-        temp1 = []
-        temp2 = []
-        for j in range(1,4):
-            temp1.append(np.unit16(input[j][i]))
-            temp2.append(np.unit16(target[j][i]))
-        input_ycbcr.append(temp1)
-        target_ycbcr.append(temp2)
 
-    input_rgb = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input_ycbcr]
+    input_ycbcr =[]
+    target_ycbcr = []
+    print (input.shape, target.shape) # (3x360x480, 3x360x480)
+
+    for i in range(360):
+        for j in range(480):
+            temp1 = []
+            temp2 = []
+            for k in range(0,3):
+                temp1.append(input[k][i][j])
+                temp2.append(target[k][i][j])
+            input_ycbcr.append(temp1)
+            target_ycbcr.append(temp2)
+    print(len(input_ycbcr))
+    input_ycbcr = [[x[0],x[2], x[1]] for x in input_ycbcr]
+    target_ycbcr = [[x[0],x[2], x[1]] for x in target_ycbcr]
+    input_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB) for x in input_ycbcr]
     input_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in input_rgb]
 
-    target_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB ) for x in target_ycbcr]
+    target_rgb = [cv2.cvtColor(np.array([[x]]), cv2.COLOR_YCrCb2RGB) for x in target_ycbcr]
     target_xyz = [cv2.cvtColor(np.array(x), cv2.COLOR_RGB2XYZ) for x in target_rgb]
-    
+
     mse = (np.square(np.array(input_xyz) - np.array(target_xyz))).mean(axis=0)
-    loss = 10*np.log(10*len(input_ycbcr)/np.sum(mse))
+    loss = 10*np.log(10*(2**10)/np.sum(mse))
     ### need modification
 
-    loss = Variable(torch.Tensor(loss), requires_grad=True)
+    loss = Variable(torch.Tensor(np.array(loss)), requires_grad=True)
 
     return loss
