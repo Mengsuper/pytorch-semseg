c5ea0e2c32d830527f0fbc8b4c6a1b330ae303bd
Unknown <teresazhao13@gmail.com>
2018-11-22 19:55:26 -0800

    quick commit

--- ptsemseg/loss/pytorch_ssim/__init__.py
+++ ptsemseg/loss/pytorch_ssim/__init__.py
@@ -30,12 +30,12 @@ def _ssim(img1, img2, window, window_size, channel, size_average = True):
     C2 = 0.03**2
 
     ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
-    print(ssim_map)
-
 
     if size_average:
+        print(ssim_map.mean())
         return ssim_map.mean()
     else:
+        print(ssim_map.mean(1).mean(1).mean(1))
         return ssim_map.mean(1).mean(1).mean(1)
 
 class SSIM(torch.nn.Module):
